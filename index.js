f442bf91dec5e62fc52f2fa7dacd3e324c1e7468
Michel Araujo <michel_araujo.ti@hotmail.com>
2016-10-24 23:30:06 -0200

    Formatted code to pattern airbnb and fix bug in test

--- index.js
+++ index.js
@@ -8,40 +8,39 @@
  */
 class DateInterval {
 
-    /**
-     * @constructor
-     * @param {String} date start date range
-     * @author Michel Araujo <araujo_michel@yahoo.com.br>
-     */
-	constructor(date) {
-	    if (date === undefined) {
-	        this._date = new Date();
-		} else {
-            this._date = new Date(date);
-        }
-  	}
-
-    /**
-     * Return the date concerning the interval
-     *
-     * @param {int} interval Interval Number of days
-     * @author Michel Araujo <araujo_michel@yahoo.com.br>
-     * @returns {Object}
-     */
-	setDays(interval) {
-	    const newDay = this._date.getUTCDate() + interval;
-        this._date.setUTCDate(newDay);
-
-        return this;
-	}
-
-    /**
-     * If the object is converted to string returns this standard format
-     * @returns {string}
-     */
-	toString() {
-        return this._date.getUTCDate()+'/'+(this._date.getUTCMonth()+1)+'/'+this._date.getUTCFullYear();
+  /**
+   * @constructor
+   * @param {String} date start date range
+   * @author Michel Araujo <araujo_michel@yahoo.com.br>
+   */
+  constructor(date) {
+    if (date === undefined) {
+      this.date = new Date();
+    } else {
+      this.date = new Date(date);
     }
+  }
+
+  /**
+   * Return the date concerning the interval
+   *
+   * @param {int} interval Interval Number of days
+   * @author Michel Araujo <araujo_michel@yahoo.com.br>
+   * @returns {Object}
+   */
+  setDays(interval) {
+    const newDay = this.date.getUTCDate() + interval;
+    this.date.setUTCDate(newDay);
+    return this;
+  }
+
+  /**
+   * If the object is converted to string returns this standard format
+   * @returns {string}
+   */
+  toString() {
+    return this.date.getUTCDate() + '/' + (this.date.getUTCMonth() + 1) + '/' + this.date.getUTCFullYear();
+  }
 }
 
 module.exports = DateInterval;
