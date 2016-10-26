7703164a3726b96feda0367b3608129f5834fd64
Michel Araujo <michel_araujo.ti@hotmail.com>
2016-10-26 20:34:08 -0200

    Made the format with hours

--- index.js
+++ index.js
@@ -13,11 +13,19 @@ class DateInterval {
    * @param {String} date start date range
    * @author Michel Araujo <araujo_michel@yahoo.com.br>
    */
-  constructor(date) {
+  constructor(date, utc) {
     if (date === undefined) {
       this.date = new Date();
     } else {
       this.date = new Date(date);
+    }
+
+    if (utc === undefined) {
+      this.utc = true;
+    } else if (utc === 'local') {
+      this.utc = false;
+    } else {
+      this.utc = true;
     }
   }
 
@@ -29,12 +37,92 @@ class DateInterval {
    * @returns {Object}
    */
   setDays(interval) {
-    const newDay = this.date.getUTCDate() + interval;
-    this.date.setUTCDate(newDay);
+    if (this.utc) {
+      const newDay = this.date.getUTCDate() + interval;
+      this.date.setUTCDate(newDay);
+    } else {
+      const newDay = this.date.getDate() + interval;
+      this.date.setDate(newDay);
+    }
     return this;
   }
 
   format(format) {
+    if (this.utc) {
+      return this.getFormatUTC(format);
+    } else {
+
+    }
+  }
+
+  /**
+   * If the object is converted to string returns this standard format
+   * @returns {string}
+   */
+  toString() {
+    return this.date.getUTCDate() + '/' + (this.date.getUTCMonth() + 1) + '/' + this.date.getUTCFullYear();
+  }
+
+  getFormatUTC(format) {
+    switch (format) {
+      case 'Y-M': {
+        return this.date.getUTCFullYear() + '-' + this.addZeroInMonthUTC();
+      }
+      case 'y-m': {
+        return this.date.getUTCFullYear() + '-' + (this.date.getUTCMonth() + 1);
+      }
+      case 'M-Y': {
+        return this.addZeroInMonthUTC() + '-' + this.date.getUTCFullYear();
+      }
+      case 'm-y': {
+        return (this.date.getUTCMonth() + 1) + '-' + this.date.getUTCFullYear();
+      }
+      case 'Y-M-D': {
+        return this.date.getUTCFullYear() + '-' + this.addZeroInMonthUTC() + '-' + this.addZeroInDayUTC();
+      }
+      case 'D-M-Y': {
+        return this.addZeroInDayUTC() + '-' + this.addZeroInMonthUTC() + '-' + this.date.getUTCFullYear();
+      }
+      case 'Y-M-D h:m:s': {
+        return this.date.getUTCFullYear() + '-' + this.addZeroInMonthUTC() + '-' + this.addZeroInDayUTC()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
+      case 'D-M-Y h:m:s': {
+        return this.addZeroInDayUTC() + '-' + this.addZeroInMonthUTC() + '-' + this.date.getUTCFullYear()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
+      case 'Y/M': {
+        return this.date.getUTCFullYear() + '/' + this.addZeroInMonthUTC();
+      }
+      case 'y/m': {
+        return this.date.getUTCFullYear() + '/' + (this.date.getUTCMonth() + 1);
+      }
+      case 'M/Y': {
+        return this.addZeroInMonthUTC() + '/' + this.date.getUTCFullYear();
+      }
+      case 'm/y': {
+        return (this.date.getUTCMonth() + 1) + '/' + this.date.getUTCFullYear();
+      }
+      case 'Y/M/D': {
+        return this.date.getUTCFullYear() + '/' + this.addZeroInMonthUTC() + '/' + this.addZeroInDayUTC();
+      }
+      case 'D/M/Y': {
+        return this.addZeroInDayUTC() + '/' + this.addZeroInMonthUTC() + '/' + this.date.getUTCFullYear();
+      }
+      case 'Y/M/D h:m:s': {
+        return this.date.getUTCFullYear() + '/' + this.addZeroInMonthUTC() + '/' + this.addZeroInDayUTC()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
+      case 'D/M/Y h:m:s': {
+        return this.addZeroInDayUTC() + '/' + this.addZeroInMonthUTC() + '/' + this.date.getUTCFullYear()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
+      default:
+        throw new Error('Format of the date invalid');
+    }
+  }
+
+  getFormatLocal(format) {
     switch (format) {
       case 'Y-M': {
         return this.date.getUTCFullYear() + '-' + this.addZeroInMonth();
@@ -54,20 +142,46 @@ class DateInterval {
       case 'D-M-Y': {
         return this.addZeroInDay() + '-' + this.addZeroInMonth() + '-' + this.date.getUTCFullYear();
       }
+      case 'Y-M-D h:m:s': {
+        return this.date.getUTCFullYear() + '-' + this.addZeroInMonth() + '-' + this.addZeroInDay()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
+      case 'D-M-Y h:m:s': {
+        return this.addZeroInDay() + '-' + this.addZeroInMonth() + '-' + this.date.getUTCFullYear()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
+      case 'Y/M': {
+        return this.date.getUTCFullYear() + '/' + this.addZeroInMonth();
+      }
+      case 'y/m': {
+        return this.date.getUTCFullYear() + '/' + (this.date.getUTCMonth() + 1);
+      }
+      case 'M/Y': {
+        return this.addZeroInMonth() + '/' + this.date.getUTCFullYear();
+      }
+      case 'm/y': {
+        return (this.date.getUTCMonth() + 1) + '/' + this.date.getUTCFullYear();
+      }
+      case 'Y/M/D': {
+        return this.date.getUTCFullYear() + '/' + this.addZeroInMonth() + '/' + this.addZeroInDay();
+      }
+      case 'D/M/Y': {
+        return this.addZeroInDay() + '/' + this.addZeroInMonth() + '/' + this.date.getUTCFullYear();
+      }
+      case 'Y/M/D h:m:s': {
+        return this.date.getUTCFullYear() + '/' + this.addZeroInMonth() + '/' + this.addZeroInDay()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
+      case 'D/M/Y h:m:s': {
+        return this.addZeroInDay() + '/' + this.addZeroInMonth() + '/' + this.date.getUTCFullYear()
+          + ' ' + this.date.getUTCHours() + ':' + this.date.getUTCMinutes() + ':' + this.date.getUTCSeconds();
+      }
       default:
         throw new Error('Format of the date invalid');
     }
   }
 
-  /**
-   * If the object is converted to string returns this standard format
-   * @returns {string}
-   */
-  toString() {
-    return this.date.getUTCDate() + '/' + (this.date.getUTCMonth() + 1) + '/' + this.date.getUTCFullYear();
-  }
-
-  addZeroInMonth() {
+  addZeroInMonthUTC() {
     let month = this.date.getUTCMonth() + 1;
     if (month < 10) {
       month = '0' + month;
@@ -75,7 +189,7 @@ class DateInterval {
     return month;
   }
 
-  addZeroInDay() {
+  addZeroInDayUTC() {
     let day = this.date.getUTCDate();
     if (day < 10) {
       day = '0' + day;
